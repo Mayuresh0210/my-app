a88e182dac48fcf7b0324ab74e46a46c59352013
Mayuresh0210 <121124304+Mayuresh0210@users.noreply.github.com>
2022-12-23 19:42:06 +0530

    dynamic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 
 
+import {useState} from "react";
+
+
 function App() {
 
-  let img={color:"white", background:"purple"};
+  let [theme, setTheme] = useState("secondary");
+
+  let primarytheme= () => {
+    theme="primary";
+    setTheme(theme);
+  };
+  
+  let successtheme= () => {
+    theme="success";
+    setTheme(theme);
+  };
+  
+  let dangertheme= () => {
+    theme="danger";
+    setTheme(theme);
+  };
+
   return (
     <div>
       
 
-      <h1 className=" bg-secondary text-white p-3">External styling</h1>
+      <h1 className ={`bg-${theme} text-white p-3`}>External styling {theme}</h1>
+      <h1>
+        <input
+        className="btn btn-primary ms-1" type="button" value="primary color" onClick={primarytheme}/>
+        <input  className="btn btn-success ms-1 " type="button" value="success color" onClick={successtheme}/>
+        <input   className="btn btn-danger ms-1"type="button" value="danger color"  onClick={dangertheme}/>
+
+      </h1>
       
       </div>
   );
