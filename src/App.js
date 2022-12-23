e46ddff57c1125aefadce010ef76f191aaac47be
Mayuresh0210 <121124304+Mayuresh0210@users.noreply.github.com>
2022-12-23 17:35:28 +0530

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import "./App.css";
+
 
 function App() {
 
   let img={color:"white", background:"purple"};
   return (
     <div>
-      <h1 style={{color:"red", background:"black"}}>hello public</h1>
+      
 
-      <h1 style={img}>hello from mayuresh</h1>
-
-      <h1 class="img">hello from kadwaikars</h1>
+      <h1 className=" bg-secondary text-white">External styling</h1>
       
       </div>
   );
