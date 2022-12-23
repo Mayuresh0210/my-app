0df87aa0650c1312b72bef5be07d73670fdf72cd
Mayuresh0210 <121124304+Mayuresh0210@users.noreply.github.com>
2022-12-23 16:30:30 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-
+import "./App.css";
 
 function App() {
+
+  let img={color:"white", background:"purple"};
   return (
-    <div><h1>hello public</h1></div>
+    <div>
+      <h1 style={{color:"red", background:"black"}}>hello public</h1>
+
+      <h1 style={img}>hello from mayuresh</h1>
+
+      <h1 class="img">hello from kadwaikars</h1>
+      
+      </div>
   );
 }
 
