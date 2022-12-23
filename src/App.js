759e2ca3964fbdf6430871cc45ab55f77e388694
Mayuresh0210 <121124304+Mayuresh0210@users.noreply.github.com>
2022-12-23 20:57:48 +0530

    updaated

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {useState} from "react";
 function App() {
 
   let [theme, setTheme] = useState("secondary");
+  let [list] = useState([1,2,3,4,])
 
   let primarytheme= () => {
     theme="primary";
@@ -23,17 +24,26 @@ function App() {
   };
 
   return (
-    <div>
-      
 
+    <div>
+    <div>
       <h1 className ={`bg-${theme} text-white p-3`}>External styling {theme}</h1>
       <h1>
         <input
         className="btn btn-primary ms-1" type="button" value="primary color" onClick={primarytheme}/>
         <input  className="btn btn-success ms-1 " type="button" value="success color" onClick={successtheme}/>
         <input   className="btn btn-danger ms-1"type="button" value="danger color"  onClick={dangertheme}/>
-
       </h1>
+      </div>
+        
+
+        {list.map((item) => (
+               <div className={`alert alert-${theme} my-6`}>
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus adipisci animi perspiciatis commodi,
+             </div>
+
+        ))};
+      
       
       </div>
   );
