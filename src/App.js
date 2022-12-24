9665f18f8cc17148ce67dc311fa33dc3ef8ed510
Mayuresh0210 <121124304+Mayuresh0210@users.noreply.github.com>
2022-12-24 17:12:36 +0530

    capture the input

--- src/App.js
+++ src/App.js
@@ -2,36 +2,32 @@
 
 import {useState} from "react";
 
+function App(){
 
-function App() {
+  let [Message , setMessage] = useState("hello world");
+
+  let updatemessage = (e)=>{
+
+    setMessage(e.target.value);
+  };
+
+return(
+
+  <div>
+    <h1>when to input </h1>
+
+    <input type="text" placeholder="enter a message"  value={Message} onChange={updatemessage}/>
+    <input type="button" value="click on"/>
+
+    <h1>{Message}</h1>
+
+
+  </div>
+
   
-  let cl1 =() => {
 
-    console.log("hello");
-  };
-  let cl2 =(e) => {
-    console.log(e);
-  };
+);
 
-  //custom parameter
-  let cl3 =(p1) => {
-    console.log(p1);
-  };
-
-  return (
-
-  
-    <div>
-      <h1>btn styling </h1>
-      <h1>
-        <input
-        className="btn btn-primary ms-1" type="button" value="cl 1" onClick={cl1}/>
-        <input  className="btn btn-success ms-1 " type="button" value="cl 2r" onClick={cl2}/>
-        <input   className="btn btn-danger ms-1"type="button" value="danger cl 3"  onClick={() => cl3("danger")}/>
-      </h1>
-      </div>
-      
-  );
 }
 
 export default App;
