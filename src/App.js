cba6865faf1a8875c432facfc64a923d06733992
Mayuresh0210 <121124304+Mayuresh0210@users.noreply.github.com>
2022-12-25 01:18:39 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,50 +1,28 @@
-
-import {useState} from "react";
+import axios from "axios";
+import { useState } from "react";
 
 function App(){
-  let [Message, setMessage] = useState("hello");
 
-  let [list, setlist] = useState([{Message:"delhi" ,Messagetime: new Date()}, {Message:"mumbai",Messagetime: new Date()}, {Message:"pune",Messagetime: new Date()}]);
-  
+  let[title] = useState("api");
+  let [messagelist, setMessagelist] = useState([]);
 
-let updateinputmsg=(e) =>{
-  Message=e.target.value;
-  setMessage(Message);
-}
-
-  let addmsg= () => {
-    let Newmessage = {Message: Message, Messagetime: new Date()};
-    list= [Newmessage, ...list];
-    setlist(list);
+  let getallmsg= async() => {
+   let  url=`http://localhost:3001//messages`;
+   let response =  await  axios.get(url);
+     console.log(response);
+     messagelist=[...response.data];
+     setMessagelist(messagelist);
   };
   return(
-  <div>
+<div>
+  <h1>integration</h1>
+  
+  <input type="button" value="ajax call" onClick={getallmsg} />
+  
+  
+  </div>
 
-     
-     <h1 className="bg-danger text-white p-2 sticky-top">map demo</h1>
-    
-
-    <div className="d-flex">
-     <input type="text" value={Message} onChange={updateinputmsg}
-     placeholder="enter message" />
-     <input className="btn btn-danger ms-3" type="button" value="add msg" onClick={addmsg} />
-     </div> 
-    { list.map((item , index ) => (
-    
-  <div key={index} className="d-flex my-1">
-
-   
-
-    <div className="badge text-bg-danger">{item.Message}
-    <span className="ms-5">{item.Messagetime.getHours()} : {item.Messagetime.getMinutes()}</span></div>
-   </div>
-     ))}  
-</div>
   );
-  
-  
-  
-  
 }
 
 export default App;
