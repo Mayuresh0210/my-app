26785912ae91cdb0d0dc0c82169405b8cfd484ad
Mayuresh0210 <121124304+Mayuresh0210@users.noreply.github.com>
2022-12-24 18:12:52 +0530

    map function with array of object

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import {useState} from "react";
 
 function App(){
 
-  let [list, setlist] = useState(["delhi", "mumbai", "pune"]);
+  let [list, setlist] = useState([{city:"delhi" ,Messagetime: new Date()}, {city:"mumbai",Messagetime: new Date()}, {city:"pune",Messagetime: new Date()}]);
 
   return(
   <div>
-     <h1>map demo</h1>
+     <h1 className="bg-danger text-white p-2 sticky-top">map demo</h1>
 
-    { list.map((item) => (<h1 className="alert alert-danger">{item}</h1>))}  
-   
-     
+    { list.map((item) => (
+    
+  <div className="d-flex my-1">
+
+    <div className="badge text-bg-danger">{item.city}
+    <span className="ms-5">{item.Messagetime.getHours()} : {item.Messagetime.getMinutes()}</span></div>
+   </div>
+     ))}  
 </div>
   );
   
