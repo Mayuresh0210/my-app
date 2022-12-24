27989bb1f5528d4b0392ab0a678e1a0c1483c3f9
Mayuresh0210 <121124304+Mayuresh0210@users.noreply.github.com>
2022-12-24 19:01:08 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,20 +2,33 @@
 import {useState} from "react";
 
 function App(){
+  let [Message, setMessage] = useState("hello");
 
   let [list, setlist] = useState([{Message:"delhi" ,Messagetime: new Date()}, {Message:"mumbai",Messagetime: new Date()}, {Message:"pune",Messagetime: new Date()}]);
   
+
+let updateinputmsg=(e) =>{
+  Message=e.target.value;
+  setMessage(Message);
+}
+
   let addmsg= () => {
-    let Newmessage = {Message: "chill", Messagetime: new Date()};
+    let Newmessage = {Message: Message, Messagetime: new Date()};
     list= [Newmessage, ...list];
     setlist(list);
   };
   return(
   <div>
+
+     
      <h1 className="bg-danger text-white p-2 sticky-top">map demo</h1>
+    
 
-     <input type="button" value="addmsg" onClick={addmsg} />
-
+    <div className="d-flex">
+     <input type="text" value={Message} onChange={updateinputmsg}
+     placeholder="enter message" />
+     <input className="btn btn-danger ms-3" type="button" value="add msg" onClick={addmsg} />
+     </div> 
     { list.map((item , index ) => (
     
   <div key={index} className="d-flex my-1">
