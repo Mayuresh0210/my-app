9e90136763f330f1e36b41e2597ee065853a321b
Mayuresh0210 <121124304+Mayuresh0210@users.noreply.github.com>
2022-12-24 18:40:01 +0530

    static message

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import {useState} from "react";
 
 function App(){
 
-  let [list, setlist] = useState([{city:"delhi" ,Messagetime: new Date()}, {city:"mumbai",Messagetime: new Date()}, {city:"pune",Messagetime: new Date()}]);
-
+  let [list, setlist] = useState([{Message:"delhi" ,Messagetime: new Date()}, {Message:"mumbai",Messagetime: new Date()}, {Message:"pune",Messagetime: new Date()}]);
+  
+  let addmsg= () => {
+    let Newmessage = {Message: "chill", Messagetime: new Date()};
+    list= [Newmessage, ...list];
+    setlist(list);
+  };
   return(
   <div>
      <h1 className="bg-danger text-white p-2 sticky-top">map demo</h1>
+
+     <input type="button" value="addmsg" onClick={addmsg} />
 
     { list.map((item , index ) => (
     
   <div key={index} className="d-flex my-1">
 
-    <div className="badge text-bg-danger">{item.city}
+   
+
+    <div className="badge text-bg-danger">{item.Message}
     <span className="ms-5">{item.Messagetime.getHours()} : {item.Messagetime.getMinutes()}</span></div>
    </div>
      ))}  
