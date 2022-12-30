d3b08091815465ae21972851f8954fad019867cd
Mayuresh0210 <121124304+Mayuresh0210@users.noreply.github.com>
2022-12-30 16:19:29 +0530

    post done

--- src/App.js
+++ src/App.js
@@ -5,19 +5,42 @@ function App(){
 
   let[title] = useState("api");
   let [messagelist, setMessagelist] = useState([]);
+  
 
   let getallmsg= async() => {
-   let  url=`http://localhost:3001//messages`;
+   let  url=`http://localhost:3002/messages`;
    let response =  await  axios.get(url);
      console.log(response);
      messagelist=[...response.data];
      setMessagelist(messagelist);
+   
   };
+
+  let createnewmsg= async() => {
+    let url =`http://localhost:3002/message`;
+
+    let data ={id: "48", name: "mayuri" , date : new Date()}
+    
+    await axios.post(url , data); 
+
+  };
+
+ 
   return(
 <div>
   <h1>integration</h1>
   
-  <input type="button" value="ajax call" onClick={getallmsg} />
+  <input type="button" value="ajax get call" onClick={getallmsg}
+   />
+
+  {messagelist.map((item) => (
+
+    <div>{item.mayur}</div>
+    
+
+  ))}
+  
+  <input type="button" value="ajax get call" onClick={createnewmsg}/>
   
   
   </div>
